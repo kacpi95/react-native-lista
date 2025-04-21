cc45b449f3add7445885d556e3c44eef8ebe2ca9
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-04-21 17:12:56 +0200

    Add function to App and Add files components

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, SafeAreaView, StyleSheet, Button, View } from 'react-native';
+
+import { ProductsList } from './components/ProductsList';
+import { ProductInput } from './components/ProductInput';
+import { SortButton } from './components/SortButton';
+import { useList } from './components/UseList';
 
 export default function App() {
+  const {
+    list,
+    addItem,
+    setBought,
+    removeBought,
+    currentSort,
+    toggleSort,
+    removeItem,
+  } = useList([]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.title}>
+        <Text style={styles.brand}>Lista zakupów</Text>
+        {list.length > 0 && (
+          <SortButton sort={currentSort} onPush={toggleSort} />
+        )}
+      </View>
+      <ProductsList list={list} setBought={setBought} removeItem={removeItem} />
+      {list.length > 0 && (
+        <Button title='usuń kupione' onPress={removeBought} color='#000' />
+      )}
+      <ProductInput onAdd={addItem} />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
